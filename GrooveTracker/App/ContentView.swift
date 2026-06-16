f85bd0cb7f38d51efaee12f56e93884885ffed07
Juan Adolfo Velazquez Reyes <velazquezjuanadolfo472@gmail.com>
2026-06-16 11:51:02 -0600

    chore: reorganize the project architecture

---
+++ GrooveTracker/App/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  GrooveTracker
+//
+//  Created by Juan Adolfo Velazquez Reyes on 16/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
